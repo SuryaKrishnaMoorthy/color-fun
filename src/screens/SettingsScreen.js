23a71dd09245c95aa421a6da46b4af392243e0c4
SURYA KRISHNA MOORTHY <ayrus2289@gmail.com>
2019-03-22 09:54:42 -0700

    update settings

--- src/screens/SettingsScreen.js
+++ src/screens/SettingsScreen.js
@@ -6,7 +6,7 @@ export default class SettingsScreen extends React.Component {
     render() {
       return (
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#8D8F8F' }}>
-          <Text style={{fontSize: 30, fontWeight: "30", color: "#ffffff"}}>Settings!</Text>
+          <Text style={{fontSize: 30, fontWeight: "bold", color: "#ffffff"}}>Settings!</Text>
         </View>
       );
     }
